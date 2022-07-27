0cc3897c6d8c8016fe05fae07c98f6eb779adc78
Jeongjihun <wlgns2234@naver.com>
2022-07-28 02:52:04 +0900

    create base.html

--- web/calldrone/app1/views.py
+++ web/calldrone/app1/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def page1(request):
-    return render(request, 'app1/index.html')
+    return render(request, 'app1/page1.html')
 
 def page2(request):
     return render(request, 'app1/page2.html')
