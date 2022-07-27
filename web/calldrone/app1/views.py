885dfd74d265df0d0cb0769a896f2ab3a7562ded
Jeongjihun <wlgns2234@naver.com>
2022-07-28 02:36:22 +0900

    urls settings and create(page1~4)

--- web/calldrone/app1/views.py
+++ web/calldrone/app1/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def page1(request):
+    return HttpResponse("page1")
+
+def page2(request):
+    return HttpResponse("page2")
+
+def page3(request):
+    return HttpResponse("page3")
+
+def page4(request):
+    return HttpResponse("page4")
