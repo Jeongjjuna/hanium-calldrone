ed707da7a635c0d35290757dc95b3b1f334bc4d3
Jeongjihun <wlgns2234@naver.com>
2022-08-22 19:25:55 +0900

    Comment : class, def에 대한 설명을 주석처리로 추가

--- web/calldrone/app1/consumers.py
+++ web/calldrone/app1/consumers.py
@@ -3,17 +3,19 @@ from collections import deque
 from asyncio import sleep
 from channels.generic.websocket import AsyncWebsocketConsumer
 
-
+# 장고 서버 실행시 WSConsumer class는 두 번 호출 된다
 class WSConsumer(AsyncWebsocketConsumer):
-    """외부 드론으로부터 들어오는 위치정보 데이터를 담고있는 Queue자료구조"""
+    print("WSConsumer class호출")
+    
+    # 외부 드론으로부터 들어오는 위치정보 데이터를 담고있는 Queue자료구조
     data_from_drone = deque([37.5665])
-    print("새로운 소켓 생성")
-
+    
+    # 클라이언트 단에서 웹소켓 연결요청을 하면 connect함수가 실행 된다
+    # 연결이 끊어지면 connect함수는 종료 된다
     async def connect(self):
+        print('확인용')
         await self.accept()
         while True:
             if self.data_from_drone:
                 await self.send(json.dumps({'message' : self.data_from_drone.popleft()}))
                 await sleep(2)
-
-                print(self.data_from_drone)
