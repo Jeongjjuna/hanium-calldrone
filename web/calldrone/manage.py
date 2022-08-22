37b342956e30eaeeb189a68cbe0a85ebfb20bf62
Jeongjihun <wlgns2234@naver.com>
2022-08-22 19:03:38 +0900

    Refactor : WSConsumer class변수 class_var -> data_from_drone 변경

--- web/calldrone/manage.py
+++ web/calldrone/manage.py
@@ -25,16 +25,16 @@ def main():
 def threaded(client_socket, addr): 
     a = 37.5665
     while True:
-        WSConsumer.class_var.append(a)
+        WSConsumer.data_from_drone.append(a)
         time.sleep(2)
         a = a * (1.000001)
-        print(WSConsumer.class_var)
+        print(WSConsumer.data_from_drone)
 
 
 if __name__ == '__main__':
     """
     드론 - 서버 소켓통신 시작하는 쓰레드 함수
-    WSconsumer 클래스의 변수에 접근하여 통신이 필요한지 아닌지 제어해 줄 수 있다.
+    WSconsumer 클래스의 변수(data_from_drone)에 접근하여 드론으로부터의 실시간 데이터를 전달해줄 수 있다.
     """
     start_new_thread(threaded, (0, 1))
     
