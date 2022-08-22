25d7f4fc9c589b8b00f7639759e0105e81f24572
Jeongjihun <wlgns2234@naver.com>
2022-08-22 23:18:06 +0900

    Comment : 페이로드정보 post방식으로 처리과정 기술

--- web/calldrone/app1/views.py
+++ web/calldrone/app1/views.py
@@ -6,6 +6,19 @@ def page1(request):
     return render(request, 'app1/page1.html')
 
 def page2(request):
+    '''
+    만약 post정보가 들어온다면
+        1. 목적지정보(위치, 구, 상세주소)
+        2. 원하는 배송품 무게
+
+        ->
+        1. 목적지 정보 데이터로 거리를 계산한다
+        2. 드론속도정보[1,2,3,
+                    4,5,6,
+                    7,8,9]
+
+        >>>>>> 사용자에게 최적 정보를 렌더링 해줘야함
+    '''
     return render(request, 'app1/page2.html')
 
 def page3(request):
