6d2f0bfa7739f7e38a4789a72e1d681727d3d746
Jeongjihun <wlgns2234@naver.com>
2022-08-22 19:06:22 +0900

    Refactor : 기존의 단순 a변수 데이터를 보내는 방식에서 class변수인 data_from_drone 으로부터 받아온 데이터를 보내는 방식으로 수정

--- web/calldrone/app1/consumers.py
+++ web/calldrone/app1/consumers.py
@@ -1,15 +1,19 @@
 import json
+from collections import deque
 from asyncio import sleep
-
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 
 class WSConsumer(AsyncWebsocketConsumer):
+    """외부 드론으로부터 들어오는 위치정보 데이터를 담고있는 Queue자료구조"""
+    data_from_drone = deque([37.5665])
+    print("새로운 소켓 생성")
+
     async def connect(self):
         await self.accept()
-        a = 37.5665
-        for i in range(1000):
-            await self.send(json.dumps({'message' : a}))
-            await sleep(1)
-            a = a*1.000001
-            print('debug')
+        while True:
+            if self.data_from_drone:
+                await self.send(json.dumps({'message' : self.data_from_drone.popleft()}))
+                await sleep(2)
+
+                print(self.data_from_drone)
