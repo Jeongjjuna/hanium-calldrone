94291e607480d6d4c338799a3df6d23000575779
Jeongjihun <wlgns2234@naver.com>
2022-09-26 22:43:47 +0900

    Test : WSConsumer 클래스 생성시(서버 실행시) print() 해주는 구문을 추가하였습니다.

--- web/calldrone/app1/consumers.py
+++ web/calldrone/app1/consumers.py
@@ -3,9 +3,9 @@ from collections import deque
 from asyncio import sleep
 from channels.generic.websocket import AsyncWebsocketConsumer
 
-# 장고 서버 실행시 WSConsumer class는 두 번 호출 된다
+# 장고 서버 실행시 WSConsumer class가 생성된다.
 class WSConsumer(AsyncWebsocketConsumer):
-    print("WSConsumer class호출")
+    print("WSConsumer class생성완료")
     
     # 외부 드론으로부터 들어오는 위치정보 데이터를 담고있는 Queue자료구조
     data_from_drone = deque([37.5665])
@@ -13,7 +13,7 @@ class WSConsumer(AsyncWebsocketConsumer):
     # 클라이언트 단에서 웹소켓 연결요청을 하면 connect함수가 실행 된다
     # 연결이 끊어지면 connect함수는 종료 된다
     async def connect(self):
-        print('확인용')
+        print('connect함수 실행')
         await self.accept()
         while True:
             if self.data_from_drone:
