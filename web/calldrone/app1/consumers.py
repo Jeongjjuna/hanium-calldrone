7723f96b1b4afe36aa59967cf22a646ca5a18960
Jeongjihun <wlgns2234@naver.com>
2022-09-27 00:30:45 +0900

    Feat : 사용자가 페이지 새로고침을 통하여 지도 페이지를 호출할 때 드론의 위치정보도 새로고침할 수 있도록 코드를 수정하였다. 페이지 접속 GET요청이 들어오면 connect함수가 실행되고 이때 data_from_drone 데큐를 비워주고 새로운 실시간 데이터를 받을 수 있도록 하였다.

--- web/calldrone/app1/consumers.py
+++ web/calldrone/app1/consumers.py
@@ -8,13 +8,20 @@ class WSConsumer(AsyncWebsocketConsumer):
     print("WSConsumer class생성완료")
     
     # 외부 드론으로부터 들어오는 위치정보 데이터를 담고있는 Queue자료구조
-    data_from_drone = deque([37.5665])
+    data_from_drone = deque([])
     
     # 클라이언트 단에서 웹소켓 연결요청을 하면 connect함수가 실행 된다
     # 연결이 끊어지면 connect함수는 종료 된다
     async def connect(self):
         print('connect함수 실행')
         await self.accept()
+        
+        
+        # 사용자가 새로 지도페이지에 접속했기 때문에
+        # data_from_drone 내부값을 비워주고 새로 갱신한다.
+        print('위치정보갱신')
+        self.data_from_drone.clear()
+
         while True:
             if self.data_from_drone:
                 await self.send(json.dumps({'message' : self.data_from_drone.popleft()}))
